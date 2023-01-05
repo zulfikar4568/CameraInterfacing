c93e1b9e1ba1cdc7f87a91dd3c1013168cad9386
zulfikar4568 <isnaen70@gmail.com>
2023-01-05 20:04:58 +0700

    Add project files.

---
+++ CameraInterfacing/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace CameraInterfacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pb_Picture = new System.Windows.Forms.PictureBox();
+            this.Bt_StartCamera = new System.Windows.Forms.Button();
+            this.Bt_Capture = new System.Windows.Forms.Button();
+            this.Cb_VideoInput = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pb_Picture
+            // 
+            this.Pb_Picture.Location = new System.Drawing.Point(533, 34);
+            this.Pb_Picture.Name = "Pb_Picture";
+            this.Pb_Picture.Size = new System.Drawing.Size(517, 453);
+            this.Pb_Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb_Picture.TabIndex = 1;
+            this.Pb_Picture.TabStop = false;
+            // 
+            // Bt_StartCamera
+            // 
+            this.Bt_StartCamera.Location = new System.Drawing.Point(151, 520);
+            this.Bt_StartCamera.Name = "Bt_StartCamera";
+            this.Bt_StartCamera.Size = new System.Drawing.Size(216, 62);
+            this.Bt_StartCamera.TabIndex = 2;
+            this.Bt_StartCamera.Text = "Start Camera";
+            this.Bt_StartCamera.UseVisualStyleBackColor = true;
+            this.Bt_StartCamera.Click += new System.EventHandler(this.Bt_StartCamera_Click);
+            // 
+            // Bt_Capture
+            // 
+            this.Bt_Capture.Location = new System.Drawing.Point(687, 520);
+            this.Bt_Capture.Name = "Bt_Capture";
+            this.Bt_Capture.Size = new System.Drawing.Size(216, 62);
+            this.Bt_Capture.TabIndex = 3;
+            this.Bt_Capture.Text = "Capture";
+            this.Bt_Capture.UseVisualStyleBackColor = true;
+            this.Bt_Capture.Click += new System.EventHandler(this.Bt_Capture_Click);
+            // 
+            // Cb_VideoInput
+            // 
+            this.Cb_VideoInput.FormattingEnabled = true;
+            this.Cb_VideoInput.Location = new System.Drawing.Point(1077, 34);
+            this.Cb_VideoInput.Name = "Cb_VideoInput";
+            this.Cb_VideoInput.Size = new System.Drawing.Size(253, 24);
+            this.Cb_VideoInput.TabIndex = 4;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(22, 34);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(450, 453);
+            this.videoSourcePlayer1.TabIndex = 5;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.VideoSourcePlayer1_NewFrame_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1379, 652);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.Cb_VideoInput);
+            this.Controls.Add(this.Bt_Capture);
+            this.Controls.Add(this.Bt_StartCamera);
+            this.Controls.Add(this.Pb_Picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Pb_Picture;
+        private System.Windows.Forms.Button Bt_StartCamera;
+        private System.Windows.Forms.Button Bt_Capture;
+        private System.Windows.Forms.ComboBox Cb_VideoInput;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ CameraInterfacing/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace CameraInterfacing
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoDevice;
+        private VideoCapabilities[] snapshotCapabilities;
+        private ArrayList listCamera = new ArrayList();
+        public string pathFolder = Application.StartupPath + @"\ImageCapture\";
+        private Stopwatch stopWatch = null;
+        private static bool needSnapshot = false;
+
+        private static string _usbcamera;
+        public string usbcamera
+        {
+            get { return _usbcamera; }
+            set { _usbcamera = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetListCameraUSB();
+        }
+
+        private void GetListCameraUSB()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count != 0)
+            {
+                foreach(FilterInfo device in videoDevices)
+                {
+                    Cb_VideoInput.Items.Add(device.Name);
+                }
+            }
+            else
+            {
+                Cb_VideoInput.Items.Add("No Direct Show devices found");
+            }
+
+            Cb_VideoInput.SelectedIndex = 0;
+        }
+
+        private void Bt_StartCamera_Click(object sender, EventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void StartCamera()
+        {
+            try
+            {
+                usbcamera = Cb_VideoInput.SelectedIndex.ToString();
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count != 0)
+                {
+                    // add all devices to combo
+                    foreach (FilterInfo device in videoDevices)
+                    {
+                        listCamera.Add(device.Name);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Camera devices found");
+                }
+
+                videoDevice = new VideoCaptureDevice(videoDevices[Convert.ToInt32(usbcamera)].MonikerString);
+                snapshotCapabilities = videoDevice.SnapshotCapabilities;
+                if (snapshotCapabilities.Length == 0)
+                {
+                    MessageBox.Show("Camera Capture Not supported");
+                }
+                OpenVideoSource(videoDevice);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        //Delegate Untuk Capture, insert database, update ke grid 
+        public delegate void CaptureSnapshotManifast(Bitmap image);
+        public void UpdateCaptureSnapshotManifast(Bitmap image)
+        {
+            try
+            {
+                needSnapshot = false;
+                Pb_Picture.Image = image;
+                Pb_Picture.Update();
+
+                string namaImage = "sampleImage";
+                string nameCapture = namaImage + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".bmp";
+                if (Directory.Exists(pathFolder))
+                {
+                    Pb_Picture.Image.Save(pathFolder + nameCapture, ImageFormat.Bmp);
+                }
+                else
+                {
+                    Directory.CreateDirectory(pathFolder);
+                    Pb_Picture.Image.Save(pathFolder + nameCapture, ImageFormat.Bmp);
+                }
+            }
+            catch { }
+        }
+        public void OpenVideoSource(IVideoSource source)
+        {
+            try
+            {
+                // set busy cursor
+                this.Cursor = Cursors.WaitCursor;
+                // stop current video source
+                CloseCurrentVideoSource();
+                // start new video source
+                videoSourcePlayer1.VideoSource = source;
+                videoSourcePlayer1.Start();
+                // reset stop watch
+                stopWatch = null;
+                this.Cursor = Cursors.Default;
+            }
+            catch { }
+        }
+
+        private void Bt_Capture_Click(object sender, EventArgs e)
+        {
+            needSnapshot = true;
+        }
+        private void VideoSourcePlayer1_NewFrame_1(object sender, ref Bitmap image)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                Graphics g = Graphics.FromImage(image);
+                // paint current time
+                SolidBrush brush = new SolidBrush(Color.Red);
+                g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+                brush.Dispose();
+                if (needSnapshot)
+                {
+                    this.Invoke(new CaptureSnapshotManifast(UpdateCaptureSnapshotManifast), image);
+                }
+                g.Dispose();
+            }
+            catch
+            { }
+        }
+        public void CloseCurrentVideoSource()
+        {
+            try
+            {
+                if (videoSourcePlayer1.VideoSource != null)
+                {
+                    videoSourcePlayer1.SignalToStop();
+                    // wait ~ 3 seconds
+                    for (int i = 0; i < 30; i++)
+                    {
+                        if (!videoSourcePlayer1.IsRunning)
+                            break;
+                        System.Threading.Thread.Sleep(100);
+                    }
+                    if (videoSourcePlayer1.IsRunning)
+                    {
+                        videoSourcePlayer1.Stop();
+                    }
+                    videoSourcePlayer1.VideoSource = null;
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ExitCamera();
+            Thread.Sleep(1000);
+        }
+
+        private void ExitCamera()
+        {
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1 = null;
+        }
+    }
+}
